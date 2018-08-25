2805bd2e6a20b0d01a1b87c285291223b382a356
gaby <gaby_dva_@hotmail.com>
2018-08-25 11:09:12 -0600

    Agregado un box a plantilla master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -366,7 +366,29 @@ desired effect
       <!--------------------------
         | Your Page Content Here |
         -------------------------->
-        @yield('contenido')
+         <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Title</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+          @yield('contenido')
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+          Footer
+        </div>
+        <!-- /.box-footer-->
+      </div>
+      <!-- /.box -->
 
     </section>
     <!-- /.content -->
