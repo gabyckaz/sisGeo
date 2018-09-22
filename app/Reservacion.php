6dd6001f7881c13be4d2cec754e76a7ad9f065af
gabyckaz <gaby_dva_@hotmail.com>
2018-09-22 12:53:05 -0600

    Prototipo de reservacion

---
+++ app/Reservacion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservacion extends Model
+{
+
+    //aqui se especifica el nombre de la tabla relacionada al modelo
+    protected $table = 'Reservacion';
+
+    //Para que no se greguen los atributos automaticos de updated_at y created_at
+    public $timestamps = false;
+
+    //Cambiando el campo por defecto id a uno personalizado
+    protected $primaryKey = 'IdReservacion';
+
+    //Relación con Turista
+    public function turista()
+    {
+      return $this->belongsTo('App\Turista','IdTurista');//Modelo y llave foránea
+    }
+
+    public function paquete()
+    {
+      return $this->belongsTo('App\Paquete','IdPaquete');//Modelo y llave foránea
+    }
+}
