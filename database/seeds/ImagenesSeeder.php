e7a3aa243dcbe5c53bd73a556db8693a865d6b04
Lizzie11 <liz_krina@hotmail.com>
2019-01-25 16:36:38 -0600

    seeder de imagenes

--- database/seeds/ImagenesSeeder.php
+++ database/seeds/ImagenesSeeder.php
@@ -21,5 +21,55 @@ class ImagenesSeeder extends Seeder
         'created_at'=>date('Y-m-d H:m:s'),
         'updated_at'=>date('Y-m-d H:m:s')
       ]);
+      Imagen::create([
+        'idimagenpaqueteturistico'=>2,
+        'imagen1'=>'http://nebula.wsimg.com/a72d096a8b13e9ae17590bd1fe866314?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen2'=>'http://nebula.wsimg.com/4157d3dd0c864717888d1c6568b128a0?AccessKeyId=028FA07779C6F82B5AD2&disposition=0&alloworigin=1',
+        'imagen3'=>'http://nebula.wsimg.com/8cc68f958658637407a8cb7703f0a6a6?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen4'=>'https://drive.google.com/file/d/18arQ28BEduqNC7OGaXuUlwFlumOehTtu/view',
+        'id_paquete'=>2,
+        'created_at'=>date('Y-m-d H:m:s'),
+        'updated_at'=>date('Y-m-d H:m:s')
+      ]);
+      Imagen::create([
+        'idimagenpaqueteturistico'=>3,
+        'imagen1'=>'http://nebula.wsimg.com/a72d096a8b13e9ae17590bd1fe866314?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen2'=>'http://nebula.wsimg.com/4157d3dd0c864717888d1c6568b128a0?AccessKeyId=028FA07779C6F82B5AD2&disposition=0&alloworigin=1',
+        'imagen3'=>'http://nebula.wsimg.com/8cc68f958658637407a8cb7703f0a6a6?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen4'=>'https://drive.google.com/file/d/18arQ28BEduqNC7OGaXuUlwFlumOehTtu/view',
+        'id_paquete'=>3,
+        'created_at'=>date('Y-m-d H:m:s'),
+        'updated_at'=>date('Y-m-d H:m:s')
+      ]);
+      Imagen::create([
+        'idimagenpaqueteturistico'=>4,
+        'imagen1'=>'http://nebula.wsimg.com/a72d096a8b13e9ae17590bd1fe866314?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen2'=>'http://nebula.wsimg.com/4157d3dd0c864717888d1c6568b128a0?AccessKeyId=028FA07779C6F82B5AD2&disposition=0&alloworigin=1',
+        'imagen3'=>'http://nebula.wsimg.com/8cc68f958658637407a8cb7703f0a6a6?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen4'=>'https://drive.google.com/file/d/18arQ28BEduqNC7OGaXuUlwFlumOehTtu/view',
+        'id_paquete'=>4,
+        'created_at'=>date('Y-m-d H:m:s'),
+        'updated_at'=>date('Y-m-d H:m:s')
+      ]);
+      Imagen::create([
+        'idimagenpaqueteturistico'=>5,
+        'imagen1'=>'http://nebula.wsimg.com/a72d096a8b13e9ae17590bd1fe866314?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen2'=>'http://nebula.wsimg.com/4157d3dd0c864717888d1c6568b128a0?AccessKeyId=028FA07779C6F82B5AD2&disposition=0&alloworigin=1',
+        'imagen3'=>'http://nebula.wsimg.com/8cc68f958658637407a8cb7703f0a6a6?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen4'=>'https://drive.google.com/file/d/18arQ28BEduqNC7OGaXuUlwFlumOehTtu/view',
+        'id_paquete'=>5,
+        'created_at'=>date('Y-m-d H:m:s'),
+        'updated_at'=>date('Y-m-d H:m:s')
+      ]);
+      Imagen::create([
+        'idimagenpaqueteturistico'=>6,
+        'imagen1'=>'http://nebula.wsimg.com/a72d096a8b13e9ae17590bd1fe866314?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen2'=>'http://nebula.wsimg.com/4157d3dd0c864717888d1c6568b128a0?AccessKeyId=028FA07779C6F82B5AD2&disposition=0&alloworigin=1',
+        'imagen3'=>'http://nebula.wsimg.com/8cc68f958658637407a8cb7703f0a6a6?AccessKeyId=B5E8C3F7E00CA38BCFD7&disposition=0&alloworigin=1',
+        'imagen4'=>'https://drive.google.com/file/d/18arQ28BEduqNC7OGaXuUlwFlumOehTtu/view',
+        'id_paquete'=>6,
+        'created_at'=>date('Y-m-d H:m:s'),
+        'updated_at'=>date('Y-m-d H:m:s')
+      ]);
     }
 }
