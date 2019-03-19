53a1507802018d68b076419195e049fe4348b52f
gaby <gaby_dva_@hotmail.com>
2019-03-19 10:39:38 -0600

    commit por cambios de alguien

--- resources/views/mail/mailEnvioView.blade.php
+++ resources/views/mail/mailEnvioView.blade.php
@@ -3,19 +3,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
     <title>Notificacion Geouturismo</title>
+    <link rel="stylesheet" href="{{ asset('adminLte/bootstrap/dist/css/bootstrap.min.css') }}">
 </head>
 <body>
-    <center>
-    <p>{{ $mensaje->de }}</p>
-
-    <textarea rows="10" cols="75" readonly>{{ $mensaje->cuerpoMensaje }}</textarea>
-
-     <p>@lang("Puedes visitar el siguiente enlace para mas informacion:")  </p>
-    <p><a href="{{ $mensaje->url }} ">Nuevo Paquete</a></p>
+    <center>    
+<div class="panel panel-success">
+  <div class="panel-heading"><h1>{{ $mensaje->de }}</h1></div>
+  <div class="panel-body">
+    {{ $mensaje->cuerpoMensaje }}.
+    <p>Puedes visitar el siguiente enlace para mas informacion: </p>
+    <h3><a href="{{ $mensaje->url }} ">Nuevo Paquete Turistico</a></h3>
     <p>¡La idea de viajar es contemplar el mismo sol en un lugar diferente!</p>
     <p>¡Feliz Dia!</p>
-    <p>Fecha de recibo : {{ $mensaje->fechaEnvio }}</p>
-    </center>
-
+    <p><small>Fecha de recibo : {{ $mensaje->fechaEnvio }}<small></p>
+  </div>
+</div>
+</center>
 </body>
 </html>
